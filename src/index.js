e8ca98397f42e91f8ec2966302a8597d76f83bdd
vetneka <vetneka@yandex.ru>
2021-02-19 11:53:54 +0500

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// function check(str, bracketsConfig) {
+  const closingBrackets = bracketsConfig.map(([, closeBracket]) => closeBracket);
+  const correctBracketsPairs = bracketsConfig.map(([openBracket, closeBracket]) => `${openBracket}${closeBracket}`);
+  const stack = [];
+
+  for (const bracket of str) {
+    const stackLast = stack[stack.length - 1];
+    const currentPairs = `${stackLast}${bracket}`;
+    if (closingBrackets.includes(bracket) && correctBracketsPairs.includes(currentPairs)) {
+      stack.pop();
+    } else {
+      stack.push(bracket);
+    }
+  }
+
+  return stack.length === 0;
 }
+
+
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
